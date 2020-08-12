8b0a1f7daeac8383ca090754c9051197816b4302
huangzhikai01 <huangzhikai01@meicai.cn>
2020-08-12 17:09:29 +0800

    优化redisTemplate组件兼容问题

--- src/main/java/com/logpolice/infrastructure/config/LogpoliceRedisTemplateAutoConfiguration.java
+++ src/main/java/com/logpolice/infrastructure/config/LogpoliceRedisTemplateAutoConfiguration.java
@@ -4,8 +4,10 @@ import com.logpolice.infrastructure.utils.redis.RedisTemplateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisTemplate;
 
 /**
@@ -17,12 +19,13 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 @AutoConfigureAfter({LogpoliceMailAutoConfiguration.class, LogpoliceDingDingAutoConfiguration.class})
 @ConditionalOnClass(RedisTemplate.class)
+@Import(RedisAutoConfiguration.class)
 public class LogpoliceRedisTemplateAutoConfiguration {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate redisTemplate;
 
     @Autowired
-    public LogpoliceRedisTemplateAutoConfiguration(RedisTemplate<String, Object> redisTemplate) {
+    public LogpoliceRedisTemplateAutoConfiguration(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
